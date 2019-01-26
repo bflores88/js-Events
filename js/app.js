a333d7777b9d3fa16f275e205c3e8a42ba49b9a1
kmietotwy <kmietotwy@gmail.com>
2019-01-26 13:36:42 -1000

    lunch wagon

--- js/app.js
+++ js/app.js
@@ -86,6 +86,15 @@ function changePug() {
 //6. DevLeague Lunch Wagon
 /*Add an event listener to the `menu` div element that will display and hide the dessert item after clicking on 'Today's Special'*/
 
+menu.addEventListener('click', dispDessert);
+
+function dispDessert(){
+    if(dessert.style.display === 'none'){
+        dessert.style.display = 'block';
+    } else {
+        dessert.style.display = 'none';
+    }
+}
 
 
 //7. The Purple Monster
